b27d20dd01686bb9283991c901a70dab2e259dbf
MauriMatamoros <mauricio.enrique.matamoros@gmail.com>
2020-02-19 19:41:13 -0600

    setting up client with webpack

---
+++ client/src/app.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { render } from 'react-dom'
+import './styles/styles.scss'
+
+import LandingPage from './pages/LandingPage'
+
+render(<LandingPage />, document.getElementById('root'))
